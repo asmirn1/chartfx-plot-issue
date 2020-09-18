8aaf02e0315e1b6ddab0770398ed307a80b6cbe6
Aleksandr Smirnov <smirnov09al@gmail.com>
2020-09-18 15:26:06 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,66 @@
+import de.gsi.chart.XYChart;
+import de.gsi.chart.renderer.spi.ErrorDataSetRenderer;
+import de.gsi.dataset.spi.DefaultErrorDataSet;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class App extends Application {
+
+    private static final Map<String, double[]> dataMap = Map.of(
+            "Set 1", new double[]{1, 2, 3, 4, 5},
+            "Set 2", new double[]{0, 1, 2, 3, 4},
+            "Set 3", new double[]{0, 0, 1, 2, 3},
+            "Set 4", new double[]{0, 0, 0, 1, 2},
+            "Set 5", new double[]{0, 0, 0, 0, 1});
+
+    private ListView<String> listView;
+    private ErrorDataSetRenderer renderer;
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        listView = new ListView<>();
+        listView.getItems().setAll(dataMap.keySet());
+        listView.getSelectionModel().selectedItemProperty().addListener((o, s, t) -> updateData(t));
+
+        XYChart chart = new XYChart();
+        HBox.setHgrow(chart, Priority.ALWAYS);
+        chart.setAnimated(false);
+        chart.getRenderers().clear();
+
+        renderer = new ErrorDataSetRenderer();
+        chart.getRenderers().add(renderer);
+
+
+        HBox hBox = new HBox();
+        hBox.getChildren().setAll(listView, chart);
+
+        Scene scene = new Scene(hBox, 1200, 800);
+
+        stage.setScene(scene);
+        stage.setOnCloseRequest(windowEvent -> {
+            Platform.exit();
+            System.exit(0);
+        });
+
+        stage.show();
+    }
+
+    private void updateData(String key) {
+        renderer.getDatasets().clear();
+        DefaultErrorDataSet dataSet = new DefaultErrorDataSet(key);
+        double[] data = dataMap.get(key);
+        for (int i = 0; i < data.length; ++i)
+            dataSet.add(i, data[i]);
+        renderer.getDatasets().add(dataSet);
+    }
+}
